8e25aae04c66a10b4726091e62846ab003224ee2
Greg Shanks <greg.shanks@gmail.com>
2019-12-01 20:10:46 +0000

    Populated SQL file to product tables

---
+++ db/spending_tracker.sql
@@ -0,0 +1,22 @@
+DROP TABLE transactions;
+DROP TABLE merchants;
+DROP TABLE tags;
+
+CREATE TABLE merchants
+(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR not null
+);
+
+CREATE TABLE tags
+(
+  id SERIAL PRIMARY KEY,
+  type VARCHAR not null
+);
+
+CREATE TABLE transactions
+(
+  id SERIAL PRIMARY KEY,
+  merchant_id INT REFERENCES merchants(id),
+  tag_id INT REFERENCES tag(id)
+);
