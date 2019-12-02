5628d7cb83c7b4db78761c83612d0e1df1ef23fc
Greg Shanks <greg.shanks@gmail.com>
2019-12-02 03:52:52 +0000

    Having issues with layout file, error: undefined method 'each'

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require("sinatra")
 require("sinatra/contrib/all")
-require_relative("controllers/merchants_controller")
-require_relative("controllers/tags_controller")
-require_relative("controllers/transactions_controller")
+require_relative("controllers/merchants_controller.rb")
+require_relative("controllers/tags_controller.rb")
+require_relative("controllers/transactions_controller.rb")
 
 get '/' do
   erb(:index)
