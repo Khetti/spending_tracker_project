887e927e7923599f379cc7e8cbaaa3108040a8a1
Greg Shanks <greg.shanks@gmail.com>
2019-12-02 02:42:23 +0000

    Working on index, figuring out how to access merchant/tag properties

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -5,3 +5,8 @@ require_relative("../models/transaction.rb")
 require_relative("../models/tag.rb")
 require_relative("../models/merchant.rb")
 also_reload("../models/*")
+
+get '/transactions' do
+  @transactions = Transaction.all
+  erb (:"transactions/index")
+end
