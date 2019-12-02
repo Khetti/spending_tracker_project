c418403e7b206168a41f5ebefa7154f237792e3b
Greg Shanks <greg.shanks@gmail.com>
2019-12-02 05:41:48 +0000

    Working on functionality for adding new transaction

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -11,3 +11,15 @@ get '/transactions' do
   @transactions = Transaction.all()
   erb (:"transactions/index")
 end
+
+get '/transactions/new' do
+  @merchants = Merchant.all()
+  @tags = Tag.all()
+  erb (:"transactions/new")
+end
+
+post '/transactions' do
+  transaction = Transaction.new(params)
+  transaction.save()
+  redirect to('/transactions')
+end
