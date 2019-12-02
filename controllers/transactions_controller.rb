b69f523bbb74649ab5ba8026e7cb736203754fc4
Greg Shanks <greg.shanks@gmail.com>
2019-12-02 02:22:14 +0000

    Set up requires in controllers. Layout.erb to be updated.

---
+++ controllers/transactions_controller.rb
@@ -0,0 +1,7 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require("pry")
+require_relative("../models/transaction.rb")
+require_relative("../models/tag.rb")
+require_relative("../models/merchant.rb")
+also_reload("../models/*")
