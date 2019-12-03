367997f38a53c671ea5c9f42c5623e27f40ebf87
Greg Shanks <greg.shanks@gmail.com>
2019-12-03 20:14:40 +0000

    Removed unnecessary routes from user controller.

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -3,24 +3,6 @@ require("sinatra/contrib/all")
 require_relative("../models/user.rb")
 also_reload("../models/*")
 
-# INDEX
-get '/users' do
-  @users = User.all()
-  erb (:"users/index")
-end
-
-# NEW
-get '/users/new' do
-  erb(:"users/new")
-end
-
-# CREATE
-post '/users' do
-  @user = User.new(params)
-  @user.save()
-  redirect to '/users'
-end
-
 # EDIT
 get '/users/:id/edit' do
   @user = User.find(params[:id])
